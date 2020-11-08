92ce2b8d4db011125385045bf0e31f36b014257e
LCHoffman <69278034+LCHoffman@users.noreply.github.com>
2020-11-08 11:54:55 -0500

    table schemata
    Began with the export from QuickDBD.  I changed the order for creating the tables to accomodate for correct PK and FK orders.

--- SQL-Table-Schemata.sql
+++ SQL-Table-Schemata.sql
@@ -2,6 +2,31 @@
 -- Link to schema: https://app.quickdatabasediagrams.com/?code=f2d00abcddcd38586d10#/d/huS8oq
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
+CREATE TABLE "employee" (
+    "emp_no" int   NOT NULL,
+    "emp_title" varchar(10)   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" varchar(50)   NOT NULL,
+    "last_name" varchar(50)   NOT NULL,
+    "sex" varchar(1)   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_employee" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "titles" (
+    "title_id" varchar(10)   NOT NULL,
+    "title" varchar(50)   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL
+);
 
 CREATE TABLE "departments" (
     "dept_no" varchar(10)   NOT NULL,
@@ -19,32 +44,6 @@ CREATE TABLE "dept_employee" (
 CREATE TABLE "dept_manager" (
     "dept_no" varchar(10)   NOT NULL,
     "emp_no" int   NOT NULL
-);
-
-CREATE TABLE "employee" (
-    "emp_no" int   NOT NULL,
-    "emp_title" varchar(10)   NOT NULL,
-    "birth_date" date   NOT NULL,
-    "first_name" varchar(50)   NOT NULL,
-    "last_name" varchar(50)   NOT NULL,
-    "sex" varchar(1)   NOT NULL,
-    "hire_date" date   NOT NULL,
-    CONSTRAINT "pk_employee" PRIMARY KEY (
-        "emp_no"
-     )
-);
-
-CREATE TABLE "salaries" (
-    "emp_no" int   NOT NULL,
-    "salary" int   NOT NULL
-);
-
-CREATE TABLE "titles" (
-    "title_id" varchar(10)   NOT NULL,
-    "title" varchar(50)   NOT NULL,
-    CONSTRAINT "pk_titles" PRIMARY KEY (
-        "title_id"
-     )
 );
 
 ALTER TABLE "dept_employee" ADD CONSTRAINT "fk_dept_employee_emp_no" FOREIGN KEY("emp_no")
